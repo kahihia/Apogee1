223a49c49a1fb8b1adfddede2d7229e5af20e460
Caldwell Clarke <caldwell.s.clarke@gmail.com>
2018-01-29 12:50:35 -0500

    updates parseParties, creates thumbnail decks

--- parties/api/pagination.py
+++ parties/api/pagination.py
@@ -5,6 +5,6 @@ from rest_framework import pagination
 # it allows us to paginate our api so we only load and render
 # a set amount of data each time
 class StandardResultsPagination(pagination.PageNumberPagination):
-	page_size = 3
+	page_size = 6
 	page_size_query_param = 'page_size'
 	max_page_size = 1000
