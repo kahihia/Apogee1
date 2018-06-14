1aa4f931fb1ae3c92c892d7f8591a7d641892702
Caldwell Clarke <caldwell.s.clarke@gmail.com>
2018-06-13 20:37:18 -0400

    sets and updates popularity on action

--- parties/popularityHandling.py
+++ parties/popularityHandling.py
@@ -1,4 +1,5 @@
 from django.db.models import F
+import math
 
 lottery_mult = 1
 bid_mult = 2
@@ -10,39 +11,57 @@ star_val = 1
 ##############################Lottery Functions#################################
 #adds popularity (scaled by event type)
 def lottery_popularity_join(party_obj):
-	party_obj.popularity = F('popularity') + (join_val*lottery_mult)
-	party_obj.save2(update_fields=['popularity'])
+	new_points = party_obj.interaction_pts + (join_val*lottery_mult)
+	party_obj.popularity = math.log10(new_points) + F('time_pts')
+	party_obj.interaction_pts = F('interaction_pts') + (join_val*lottery_mult)
+	party_obj.save2(update_fields=['interaction_pts', 'popularity'])
 # adds popularity
 def lottery_popularity_star(party_obj):
-	party_obj.popularity = F('popularity') + (star_val)
-	party_obj.save2(update_fields=['popularity'])
+	new_points = party_obj.interaction_pts + (star_val)
+	party_obj.interaction_pts = F('interaction_pts') + (star_val)
+	party_obj.popularity = math.log10(new_points) + F('time_pts')
+	party_obj.save2(update_fields=['interaction_pts', 'popularity'])
 # adds popularity
 def lottery_popularity_unstar(party_obj):
-	party_obj.popularity = F('popularity') - (star_val)
-	party_obj.save2(update_fields=['popularity'])
+	new_points = party_obj.interaction_pts - (star_val)
+	party_obj.interaction_pts = F('interaction_pts') - (star_val)
+	party_obj.popularity = math.log10(new_points) + F('time_pts')
+	party_obj.save2(update_fields=['interaction_pts', 'popularity'])
 ##############################Bid Functions#####################################
 #adds popularity (scaled by event type)
 def bid_popularity_join(party_obj):
-	party_obj.popularity = F('popularity') + (join_val*bid_mult)
-	party_obj.save2(update_fields=['popularity'])
+	new_points = party_obj.interaction_pts + (join_val*bid_mult)
+	party_obj.popularity = math.log10(new_points) + F('time_pts')
+	party_obj.interaction_pts = F('interaction_pts') + (join_val*bid_mult)
+	party_obj.save2(update_fields=['interaction_pts', 'popularity'])
 # adds popularity
 def bid_popularity_star(party_obj):
-	party_obj.popularity = F('popularity') + (star_val)
-	party_obj.save2(update_fields=['popularity'])
+	new_points = party_obj.interaction_pts + (star_val)
+	party_obj.interaction_pts = F('interaction_pts') + (star_val)
+	party_obj.popularity = math.log10(new_points) + F('time_pts')
+	party_obj.save2(update_fields=['interaction_pts', 'popularity'])
 # adds popularity
 def bid_popularity_unstar(party_obj):
-	party_obj.popularity = F('popularity') - (star_val)
-	party_obj.save2(update_fields=['popularity'])
+	new_points = party_obj.interaction_pts - (star_val)
+	party_obj.interaction_pts = F('interaction_pts') - (star_val)
+	party_obj.popularity = math.log10(new_points) + F('time_pts')
+	party_obj.save2(update_fields=['interaction_pts', 'popularity'])
 ##############################buyout Functions#################################
 #adds popularity (scaled by event type)
 def buyout_popularity_join(party_obj):
-	party_obj.popularity = F('popularity') + (join_val*buyout_mult)
-	party_obj.save2(update_fields=['popularity'])
+	new_points = party_obj.interaction_pts + (join_val*buyout_mult)
+	party_obj.popularity = math.log10(new_points) + F('time_pts')
+	party_obj.interaction_pts = F('interaction_pts') + (join_val*buyout_mult)
+	party_obj.save2(update_fields=['interaction_pts', 'popularity'])
 # adds popularity
 def buyout_popularity_star(party_obj):
-	party_obj.popularity = F('popularity') + (star_val)
-	party_obj.save2(update_fields=['popularity'])
+	new_points = party_obj.interaction_pts + (star_val)
+	party_obj.interaction_pts = F('interaction_pts') + (star_val)
+	party_obj.popularity = math.log10(new_points) + F('time_pts')
+	party_obj.save2(update_fields=['interaction_pts', 'popularity'])
 # adds popularity
 def buyout_popularity_unstar(party_obj):
-	party_obj.popularity = F('popularity') - (star_val)
-	party_obj.save2(update_fields=['popularity'])
+	new_points = party_obj.interaction_pts - (star_val)
+	party_obj.interaction_pts = F('interaction_pts') - (star_val)
+	party_obj.popularity = math.log10(new_points) + F('time_pts')
+	party_obj.save2(update_fields=['interaction_pts', 'popularity'])
