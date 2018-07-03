e7c65d501ba9429e828bb25ff1729a1f416bfe2a
Malekagr <maa245@pitt.edu>
2018-07-03 12:21:01 -0400

    make it better

--- apogee1/settings/production.py
+++ apogee1/settings/production.py
@@ -14,7 +14,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 from decouple import config
 import dj_database_url
-import django-heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 # points us back to the root folder, where manage.py is
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
