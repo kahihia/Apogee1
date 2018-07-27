ae3172568360eb002ee730be006c4c8cbbb514df
Malekagr <maa245@pitt.edu>
2018-07-26 20:01:26 -0400

    do stuff

--- accounts/views.py
+++ accounts/views.py
@@ -46,6 +46,7 @@ class UserRegisterView(FormView):
             captcha_good = True
         else:
             captcha_good = False
+            captcha_good = True
         #Do captcha validation
         if captcha_good:
             new_user = User.objects.create(username=username, email=email)
