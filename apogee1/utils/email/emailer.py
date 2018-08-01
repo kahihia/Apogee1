822b03d38c3d0d9dbae564c09f4cc63c2a2ca1e9
Rafael Ancheta <rafaelrancheta@gmail.com>
2018-08-01 14:39:13 -0700

    set main content to html

--- apogee1/utils/email/emailer.py
+++ apogee1/utils/email/emailer.py
@@ -1,4 +1,4 @@
-from django.core.mail import EmailMultiAlternatives
+from django.core.mail import EmailMessage
 from django.template.loader import get_template
 
 def email(subject, from_email, to_emails, template, email_data):
@@ -6,9 +6,9 @@ def email(subject, from_email, to_emails, template, email_data):
 		* template: takes a the file name of a email template in the templates directory, email dir is appended already
 		* kwargs: takes a object with keys appropriate to the message
 	"""
+
 	template = get_template('emails/' + template)
 	html_content = template.render(email_data)
-	text_content = 'Your mail client does not support HTML, you should probably move to one that does! In the meantime you have important messages on Apogee.'
-	msg = EmailMultiAlternatives(subject, html_content, from_email, [to_emails])
-	msg.attach_alternative(text_content, "text/plain")
+	msg = EmailMessage(subject, html_content, from_email, to_emails)
+	msg.content_subtype = "html"
 	msg.send()
