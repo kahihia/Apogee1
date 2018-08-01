87b4b854922f0aa0ebe7c5160524475e98ca8b27
Rafael Ancheta <rafaelrancheta@gmail.com>
2018-08-01 15:35:22 -0700

    seperate emails into base and content blocks, add footer for contact information, and add bulk precedense field

--- apogee1/utils/email/emailer.py
+++ apogee1/utils/email/emailer.py
@@ -1,5 +1,6 @@
 from django.core.mail import EmailMessage
 from django.template.loader import get_template
+from random import randint
 
 def email(subject, from_email, to_emails, template, email_data):
 	""" Utility email function
@@ -8,7 +9,8 @@ def email(subject, from_email, to_emails, template, email_data):
 	"""
 
 	template = get_template('emails/' + template)
+	email_data['email'] = ''.join(to_emails)
 	html_content = template.render(email_data)
-	msg = EmailMessage(subject, html_content, from_email, to_emails)
+	msg = EmailMessage(subject, html_content, from_email, to_emails, headers={'Precedence': 'bulk'})
 	msg.content_subtype = "html"
 	msg.send()
