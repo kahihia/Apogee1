55067926b4a3bfc0de58f2add676e14a338eea15
Malekagr <maa245@pitt.edu>
2018-08-01 15:21:08 -0400

    Added tentative user changes

--- accounts/models.py
+++ accounts/models.py
@@ -155,6 +155,8 @@ class UserProfile(models.Model):
 
 	new_notifications = models.BooleanField(default=False)
 
+	account_balance = models.DecimalField(max_digits=12,\
+	 							decimal_places=2, default=0)
 	# this is the same as calling UserProfile.objects.all()
 	# it just connects to the manager
 	objects = UserProfileManager()
