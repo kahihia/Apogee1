277da75051c4536e08880cac46c0b5adb937cd86
Malekagr <maa245@pitt.edu>
2018-08-02 16:50:12 -0400

    wow this is backwards

--- event_payment/partyTransactions.py
+++ event_payment/partyTransactions.py
@@ -4,7 +4,7 @@ from decimal import *
 
 #Reduces the user's account balance by the bid amount
 def bid_reduction(user, bid):
-	curr_balance = user.profile.account_balance - decimal.Decimal(bid)
+	curr_balance = user.profile.account_balance - decimal.Decimal(str(bid))
 	user.profile.account_balance = curr_balance
 	user.profile.save(update_fields=['account_balance'])
 #Reduces the user's account balance by the cost of the party_obj
