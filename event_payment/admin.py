08bd74c4f9e28d4b6d413542161de19d6a389908
Malekagr <maa245@pitt.edu>
2018-08-03 11:19:32 -0400

    Attempting to add new model for event_payments

--- event_payment/admin.py
+++ event_payment/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import EventPayment
+
+# this allows the admin site to access and edit the userprofile model
+admin.site.register(EventPayment)
--- event_payment/models.py
+++ event_payment/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from parties.models import Party
+from django.conf import settings
+from django.core.exceptions import ValidationError
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+# Model for bid which inludes bid amount, user (fk) and party pk (int)
+class EventPayment(models.Model):
+	payment_amount 	= models.DecimalField(max_digits=7, decimal_places=2, default=0)
+	payment_user 		= models.ForeignKey(settings.AUTH_USER_MODEL,
+					on_delete=models.CASCADE)
+	party 		= models.ForeignKey(Party,
+						on_delete=models.CASCADE, related_name="payment_object")
+	is_paid 	= models.BooleanField(default=False)
+	def __str__(self):
+		return str(self.bid_amount)
+
+
+
+# set integrity constraints for database
+	def clean(self, *args, **kwargs):
+		return super(EventPayment, self).clean(*args,**kwargs)
