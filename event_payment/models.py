3d4db7c337d4501cab55f48833dcb14d4d8f2f58
Malekagr <maa245@pitt.edu>
2018-08-06 14:04:04 -0400

    changed timedelta

--- event_payment/models.py
+++ event_payment/models.py
@@ -25,7 +25,7 @@ class EventPayment(models.Model):
 		print("I am getting scheduled________________________________________________")
 		# the pick time is set to be slightly before when the event 
 		# actully happens to allow everyone to get set up.
-		pay_time = self.party.party_time - timedelta(minutes=9)
+		pay_time = self.party.party_time + timedelta(minutes=11)
 		#pay_time = datetime.datetime.now() + timedelta(minutes=1)
 		# .astimezone(pytz.utc)
 		# brings in the pick winner method
