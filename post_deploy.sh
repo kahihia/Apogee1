4fe215add7319e54ac9aedc9120752513adb804a
Malekagr <maa245@pitt.edu>
2018-08-06 10:08:31 -0400

    migrate event_payment

--- post_deploy.sh
+++ post_deploy.sh
@@ -1,4 +1,5 @@
 python manage.py makemigrations --noinput
+python manage.py migrate event_payment
 python manage.py migrate auth --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
