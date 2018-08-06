e475d950c40bb8c2b7053d06dd8baf359e2a7ca0
Malekagr <maa245@pitt.edu>
2018-08-06 12:48:23 -0400

    added --noinput back

--- post_deploy.sh
+++ post_deploy.sh
@@ -7,4 +7,4 @@ yes | python manage.py migrate parties
 yes | python manage.py migrate event_payment
 yes | python manage.py migrate auth
 yes | python manage.py migrate
-python manage.py collectstatic --yes
+python manage.py collectstatic --noinput
