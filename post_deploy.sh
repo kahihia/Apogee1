72dcfa0cfa642f626aaa44ddf2ee5315e7fb8d03
Malekagr <maa245@pitt.edu>
2018-08-06 11:54:36 -0400

    removed --noinput

--- post_deploy.sh
+++ post_deploy.sh
@@ -1,4 +1,4 @@
-python manage.py makemigrations --noinput
-python manage.py migrate auth --noinput
-python manage.py migrate --noinputs
-python manage.py collectstatic --noinput
+python manage.py makemigrations
+python manage.py migrate auth
+python manage.py migrate
+python manage.py collectstatic
