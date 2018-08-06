d4c1f98156816683d06fcf90202f105cfd5504f7
Malekagr <maa245@pitt.edu>
2018-08-06 12:10:45 -0400

    added fake initial

--- post_deploy.sh
+++ post_deploy.sh
@@ -1,6 +1,8 @@
+
 yes | python manage.py makemigrations parties
 yes | python manage.py makemigrations event_payment
 yes | python manage.py makemigrations
+yes | python manage.py migrate --fake-initial
 yes | python manage.py migrate parties
 yes | python manage.py migrate event_payment
 yes | python manage.py migrate auth
