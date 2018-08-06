cd71c2ca8e425c6a36087ab29c7b806375eb90a4
Malekagr <maa245@pitt.edu>
2018-08-06 11:38:11 -0400

    Changed post_deploy.sh

--- post_deploy.sh
+++ post_deploy.sh
@@ -1,11 +1,4 @@
-python manage.py migrate event_payment __init__
-python manage.py migrate account zero
-python manage.py migrate parties zero
-python manage.py migrate userstatistics zero
-python manage.py migrate bids zero
-python manage.py migrate notifications zero
 python manage.py makemigrations --noinput
-python manage.py migrate event_payment
 python manage.py migrate auth --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
