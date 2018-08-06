d638cc6183263d862f1c581e83678f9acdc60e18
Malekagr <maa245@pitt.edu>
2018-08-06 12:58:17 -0400

    Called wrong object

--- event_payment/models.py
+++ event_payment/models.py
@@ -24,7 +24,7 @@ class EventPayment(models.Model):
 		print("I am getting scheduled________________________________________________")
 		# the pick time is set to be slightly before when the event 
 		# actully happens to allow everyone to get set up.
-		pay_time = self.party_time + timedelta(minutes=1)
+		pay_time = self.party.party_time + timedelta(minutes=1)
 		# .astimezone(pytz.utc)
 		# brings in the pick winner method
 		from .tasks import pay_owner
