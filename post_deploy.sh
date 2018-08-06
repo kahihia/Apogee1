38ebbf489ea7c2bc436498a8a99d21e9e420f9fe
Malekagr <maa245@pitt.edu>
2018-08-06 10:33:06 -0400

    added some zero migrations

--- post_deploy.sh
+++ post_deploy.sh
@@ -1,3 +1,9 @@
+python manage.py migrate event_payment zero
+python manage.py migrate account zero
+python manage.py migrate parties zero
+python manage.py migrate userstatistics zero
+python manage.py migrate bids zero
+python manage.py migrate notifications zero
 python manage.py makemigrations --noinput
 python manage.py migrate event_payment
 python manage.py migrate auth --noinput
