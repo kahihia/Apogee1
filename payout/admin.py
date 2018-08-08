75bee95cd583c024ad9be8693c8b0b327da74fd4
Malekagr <maa245@pitt.edu>
2018-08-08 17:19:12 -0400

    Less go

--- payout/admin.py
+++ payout/admin.py
@@ -16,14 +16,3 @@ class PayoutModelAdmin(admin.ModelAdmin):
 
 # so that you can access the model on the admin site
 admin.site.register(Payout, PayoutModelAdmin)
-
-
-payout_user 		= models.ForeignKey(
-							settings.AUTH_USER_MODEL, 
-							on_delete=models.CASCADE
-						)
-	payout_amount 		= models.DecimalField(max_digits=12, decimal_places=2, default=0)
-	time_requested		= models.DateTimeField(auto_now_add=True)
-	description 		= models.CharField(max_length=280)
-	payment_info		= models.CharField(max_length=40)
-	is_paid 			= models.BooleanField(default=False)
