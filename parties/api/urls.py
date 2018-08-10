453aa345f7f8ab8b02be9a6bc79ed6b0c9d312d0
Malekagr <maa245@pitt.edu>
2018-08-10 15:00:13 -0400

    forgetting return statement
    A+ Programming work from me

--- parties/api/urls.py
+++ parties/api/urls.py
@@ -33,6 +33,6 @@ urlpatterns = [
     path('<int:pk>/star/', StarToggleAPIView.as_view(), name='star-toggle'),
     path('<int:pk>/join/<bids>', BidAPIView.as_view(), name='bid-toggle'),
     path('<int:pk>/join/', BuyoutLotteryAPIView.as_view(), name='join-toggle'),
-    path('<int:pk>/report/', ReportAPIView.as_view(), name='report'),
+    # path('<int:pk>/report/', ReportAPIView.as_view(), name='report'),
    # url(r"^<int:pk>/join/(?P<dollar>\d+\.\d+)$", BidAPIView.as_view(), name="bid-toggle"), 
  ]
