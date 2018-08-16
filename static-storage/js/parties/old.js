81388773c18b9109f51f2187daa11641462997b0
Malekagr <maa245@pitt.edu>
2018-08-16 12:58:38 -0400

    new check icon

--- static-storage/js/parties/old.js
+++ static-storage/js/parties/old.js
@@ -32,7 +32,7 @@ function renderPartyList(partyContainerID){
   let bid_icon = '<i class="fas fa-gavel grey-color" data-toggle="tooltip" data-placement="top" title="Auction"></i>';
   let buy_icon = '<i class="fa fa-donate grey-color" data-toggle="tooltip" data-placement="top" title="Buy Now"></i>';
   let closed_icon = '<i class="fas fa-ban grey-color"></i>';
-  let check_icon = '<i class="fas fa-ban grey-color"></i>';
+  let check_icon = '<i class="fas fa-check"></i>';
 
   fetchParties();
   $('#loadmore').click(function(event){
