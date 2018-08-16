10cd5449767522644f4e7b5aed7e357eed92649f
rancheta <rafaelrancheta@gmail.com>
2018-08-16 13:00:46 -0700

    increase message history

--- eventmessages/api/pagination.py
+++ eventmessages/api/pagination.py
@@ -6,6 +6,6 @@ from rest_framework import pagination
 # a set amount of data each time
 class StandardResultsPagination(pagination.CursorPagination):
 	# this is the number of parties per API page
-	page_size = 5
+	page_size = 30
 	page_size_query_param = 'page_size'
 	ordering = '-timestamp'
