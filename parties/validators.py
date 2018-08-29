db4a028c4f3a07f74998bd3f40cabf5b2e2ec608
Malekagr <maa245@pitt.edu>
2018-08-29 15:44:09 -0400

    replaced tabs

--- parties/validators.py
+++ parties/validators.py
@@ -20,11 +20,11 @@ def validate_profanity(value):
 def is_profane(content):
 	print("_____________________________________________________________________________________")
 	with open('profanity.txt', 'r') as myfile:
-    	words=myfile.read().replace('\n', '')
-    for word in words:
-    	if word in content:
-    		return True
-    return False
+		words=myfile.read().replace('\n', '')
+	for word in words:
+		if word in content:
+			return True
+	return False
 
 
 # we need a validator to ensure that the events are not in the past
