030ca685d50983aa2d576bd6462528328275a9df
Malekagr <maa245@pitt.edu>
2018-08-29 15:42:19 -0400

    Validate Profanity

--- parties/validators.py
+++ parties/validators.py
@@ -11,6 +11,21 @@ def validate_title(value):
 		raise ValidationError('Title cannot be blank')
 	return value
 
+def validate_profanity(value):
+	content = value
+	if is_profane(content):
+		raise ValidationError('Remove offensive language')
+
+
+def is_profane(content):
+	print("_____________________________________________________________________________________")
+	with open('profanity.txt', 'r') as myfile:
+    	words=myfile.read().replace('\n', '')
+    for word in words:
+    	if word in content:
+    		return True
+    return False
+
 
 # we need a validator to ensure that the events are not in the past
 # def validate_party_time(value):
