7aee024e92dd15fae413f259d525d5eb70326a32
Malekagr <maa245@pitt.edu>
2018-09-04 17:26:47 -0400

    print statements

--- static-storage/js/notifications/notification-rendering.js
+++ static-storage/js/notifications/notification-rendering.js
@@ -1,7 +1,6 @@
 // this is the notification rendering section. it operates similarly to 
       // the party container function
       function loadNotificationContainer(notifContainerID) {
-        alert("Loading");
         // VARIABLES
          // this is the set of notifications in the api
         let notifList = [];
