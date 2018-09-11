087c078709c32dcc881b7dabe304033137085ffe
rancheta <7018054+rancheta@users.noreply.github.com>
2018-09-11 11:36:08 -0700

    Make register and login reusable and check that you can register and unregister a user

--- selenium_tests/login.py
+++ selenium_tests/login.py
@@ -7,27 +7,27 @@ host = 'http://localhost:8000'
     Base test to see if test is working at all
 """
 
-class Profile(BaseCase):
+def login(self):
+    # Test that you can login 
+    self.open(host + '/accounts/login')
+    self.update_text('#id_username', self.username)
+    self.update_text('#id_password', self.password)
+    self.click('.btn-primary-new')
+    self.wait_for_element_present("h1.text-left", timeout=2)
+
+class Login(BaseCase):
 
     username = 'test_user3'
     email = 'testuser@testuser.com'
     password = 'test_userPASS'
 
     def test_login(self):
-        # Test that you can login 
-        self.open(host + '/accounts/login')
-        self.update_text('#id_username', self.username)
-        self.update_text('#id_password', self.password)
-        self.click('.btn-primary-new')
-        self.wait_for_element_present("h1.text-left", timeout=10)
-
+        self.login(self)
+        
     def test_following_tab_on_login(self):
         # Test that you can login 
-        self.open(host + '/accounts/login')
-        self.update_text('#id_username', self.username)
-        self.update_text('#id_password', self.password)
-        self.click('.btn-primary-new')
-        self.assert_element(".home-nav ul > li > a.active", timeout=10)
+        self.login(self)
+        self.assert_element(".home-nav ul > li > a.active", timeout=2)
         highlighted_events_section = self.get_text(".home-nav a.active")
         self.assertTrue(highlighted_events_section == 'FOLLOWING')
 
--- selenium_tests/registration.py
+++ selenium_tests/registration.py
@@ -1,5 +1,7 @@
 from seleniumbase import BaseCase
 import time
+from login import login
+import pytest
 
 host = 'http://localhost:8000'
 
@@ -7,29 +9,48 @@ host = 'http://localhost:8000'
     Base test to see if test is working at all
 """
 
+def register(self):
+    self.open(host + '/register/')
+    current_url = self.get_current_url()
+    self.assertTrue("register" in current_url)
+    login_title = self.get_text("h1")
+    self.assertTrue(login_title == "Sign Up")
+    self.update_text('#id_username', self.username)
+    self.update_text('#id_email', self.email)
+    self.update_text('#id_password', self.password)
+    self.update_text('#id_password2', self.password)
+    self.click('#tos')
+    self.click('#submit')
+    time.sleep(1)
+
+def unregister(self):
+    login(self)
+    self.open(host + '/profiles/' + self.username + '/unregister/')
+    page_heading = self.get_text("h1")
+    self.assertTrue(page_heading == "Unregister")
+    self.click('.btn-danger')
+    page_heading = self.get_text("h1")
+    self.assertTrue(page_heading == "Account Deleted")
+
+
 class Register(BaseCase):
 
     username = 'test_user3'
     email = 'testuser@testuser.com'
     password = 'test_userPASS'
 
+    @pytest.mark.run(order=1)
     def test_register(self):
-        self.open(host + '/register/')
-        current_url = self.get_current_url()
-        self.assertTrue("register" in current_url)
-        login_title = self.get_text("h1")
-        self.assertTrue(login_title == "Sign Up")
-        self.update_text('#id_username', self.username)
-        self.update_text('#id_email', self.email)
-        self.update_text('#id_password', self.password)
-        self.update_text('#id_password2', self.password)
-        self.click('#tos')
-        self.click('#submit')
-        time.sleep(3)
+        register(self)
 
+    @pytest.mark.run(order=2)
     def test_user_exists(self):
+        login(self)
         self.open(host + '/profiles/' + self.username)
         profile_title = self.get_text("h2")
         self.assertTrue(profile_title == self.username)
-
+    
+    @pytest.mark.run(order=3)
+    def test_delete_user(self):
+        unregister(self)
 
