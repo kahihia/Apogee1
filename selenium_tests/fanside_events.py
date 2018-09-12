3222116cbcace2897b502bd48618ef1056cf0791
rancheta <7018054+rancheta@users.noreply.github.com>
2018-09-12 14:29:14 -0700

    remove pytest cache from git, one register on fanside events and delete confirm fill page

--- selenium_tests/fanside_events.py
+++ selenium_tests/fanside_events.py
@@ -21,7 +21,7 @@ class create_fparty(BaseCase):
 
     cur_event_link = None
 
-    @pytest.mark.run(order=2)
+    @pytest.mark.run(order=1)
     def test_create_buy(self):
         # Create as a Creator
         register(self)
@@ -59,5 +59,80 @@ class create_fparty(BaseCase):
         self.open(host + '/accounts/logout/')
 
     @pytest.mark.run(order=3)
+    def test_create_lottery(self):
+        login(self)
+        event_title = "DESTROY ALL HUMANS"
+        possible_winners = '10'
+        cost = '10.00'
+        max_entrants = '25'
+        event_desc = ("Join me in our weekly potluck to discuss the future of destroying humans."
+                      "John will bring fresh oil, but brining your own will me much appreciated. Please contact Sara "
+                      "about more controversial networking not concerning destorying humans. ")
+        self.open(host + '/events/create/')
+        self.update_text('#id_title', event_title)
+        self.update_text('#id_description', event_desc)
+        self.update_text('#id_datetime', datetime.date(2022, 11, 9).strftime('%Y-%m-%d %H:%M'))
+        self.find_element("#id_max_entrants").send_keys(3)
+        self.find_element("#id_max_entrants").send_keys(Keys.UP)
+        self.find_element("#id_max_entrants").send_keys(Keys.DOWN)
+        self.find_element("#id_max_entrants").send_keys(Keys.DOWN)
+        self.find_element("#id_max_entrants").send_keys(Keys.DOWN)
+        self.update_text("#id_num_possible_winners", possible_winners)
+        self.update_text("#id_cost", cost)
+        time.sleep(10)
+        self.click('.btn-primary-new')
+        self.assertTrue(self.get_text("h1"), event_title)
+        self.assertTrue(self.get_text("#num_joined"), "0")
+        self.assertTrue(self.get_text("#max_entrants"), max_entrants)
+        self.assertTrue(self.get_text("#num_possible_winners"), possible_winners)
+        cur_event_link = self.get_current_url()
+        self.open(host + '/accounts/logout/')
+        # Join as a User
+        fan_login(self)
+        self.open(cur_event_link)
+        self.assertTrue(self.get_text("h1"), "DESTROY ALL HUMANS")
+        self.assertTrue(self.get_text(".joinBtn"), "Join")
+        self.click(".joinBtn")
+        time.sleep(3)
+        self.assertTrue(self.get_text(".joinBtn"), "Joined")
+        self.open(host + '/accounts/logout/')
+
+    @pytest.mark.run(order=5)
+    def test_create_bid(self):
+        # Create as a Creator
+        login(self)
+        event_title = "DESTROY ALL HUMANS"
+        possible_winners = '10'
+        cost = '10.00'
+        event_desc = ("Join me in our weekly potluck to discuss the future of destroying humans."
+                      "John will bring fresh oil, but brining your own will me much appreciated. Please contact Sara "
+                      "about more controversial networking not concerning destorying humans. ")
+        self.open(host + '/events/create/')
+        self.update_text('#id_title', event_title)
+        self.update_text('#id_description', event_desc)
+        self.update_text('#id_datetime', datetime.date(2022, 11, 9).strftime('%Y-%m-%d %H:%M'))
+        self.find_element("#id_event_type").send_keys(3)
+        self.find_element("#id_event_type").send_keys(Keys.UP)
+        self.find_element("#id_event_type").send_keys(Keys.DOWN)
+        self.update_text("#id_num_possible_winners", possible_winners)
+        self.update_text("#id_cost", cost)
+        time.sleep(10)
+        self.click('.btn-primary-new')
+        self.assertTrue(self.get_text("h1"), event_title)
+        self.assertTrue(self.get_text("#num_possible_winners"), possible_winners)
+        cur_event_link = self.get_current_url()
+        self.open(host + '/accounts/logout/')
+        # Join as a User
+        fan_login(self)
+        self.open(cur_event_link)
+        self.assertTrue(self.get_text("h1"), "DESTROY ALL HUMANS")
+        self.assertTrue(self.get_text(".joinBtn"), "Join")
+        self.update_text("#bid_amount", "10.50")
+        self.click(".joinBtn")
+        time.sleep(3)
+        self.assertTrue(self.get_text(".joinBtn"), "Joined")
+        self.open(host + '/accounts/logout/')
+
+    @pytest.mark.run(order=6)
     def test_delete_creator(self):
         unregister(self)
