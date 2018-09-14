43ffc0299fab2d9f3bd67b9a49257cd73e31a721
rancheta <7018054+rancheta@users.noreply.github.com>
2018-09-14 11:28:14 -0700

    delete old media folder

--- post_deploy.sh
+++ post_deploy.sh
@@ -6,4 +6,4 @@ yes | python manage.py migrate parties
 yes | python manage.py migrate event_payment
 yes | python manage.py migrate auth
 yes | python manage.py migrate
-yes | python manage.py collectstatic
+python manage.py collectstatic --noinput
