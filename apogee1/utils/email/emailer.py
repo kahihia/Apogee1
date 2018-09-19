3dc0fe2990bb75d4c27499ed611288de557c5ff2
Malekagr <maa245@pitt.edu>
2018-09-19 19:20:26 -0400

    changed config

--- apogee1/utils/email/emailer.py
+++ apogee1/utils/email/emailer.py
@@ -1,16 +1,17 @@
 from django.core.mail import EmailMessage
 from django.template.loader import get_template
 from random import randint
+from decouple import config
 
 def email(subject, from_email, to_emails, template, email_data):
 	""" Utility email function
 		* template: takes a the file name of a email template in the templates directory, email dir is appended already
 		* kwargs: takes a object with keys appropriate to the message
 	"""
-
-	template = get_template('emails/' + template)
-	email_data['email'] = ''.join(to_emails)
-	html_content = template.render(email_data)
-	msg = EmailMessage(subject, html_content, 'Granite <' + from_email + '>', to_emails, headers={'Precedence': 'bulk'})
-	msg.content_subtype = "html"
-	msg.send()
+	if config('EMAIL_ON'):
+		template = get_template('emails/' + template)
+		email_data['email'] = ''.join(to_emails)
+		html_content = template.render(email_data)
+		msg = EmailMessage(subject, html_content, 'Granite <' + from_email + '>', to_emails, headers={'Precedence': 'bulk'})
+		msg.content_subtype = "html"
+		msg.send()
