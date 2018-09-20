94d45c08e2c58639d72fde11089133b8041e5d87
Malekagr <maa245@pitt.edu>
2018-09-20 03:47:41 -0400

    changed settings a bit

--- apogee1/settings.py
+++ apogee1/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #HEROKU
-#SECRET_KEY = '6fw*ujba!d-3^a8ez_9*da+2@bt2(-1*4@f7bjuvxas$puux_8'
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # shows debug messages in the page 
