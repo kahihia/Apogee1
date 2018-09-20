61040f568bfc9d93f197c12ae2a5d857485be7c7
Malekagr <maa245@pitt.edu>
2018-09-19 23:43:27 -0400

    config email

--- apogee1/utils/email/emailer.py
+++ apogee1/utils/email/emailer.py
@@ -8,10 +8,12 @@ def email(subject, from_email, to_emails, template, email_data):
 		* template: takes a the file name of a email template in the templates directory, email dir is appended already
 		* kwargs: takes a object with keys appropriate to the message
 	"""
-	if not config('EMAIL_ON'):
+	if config('EMAIL_ON'):
 		template = get_template('emails/' + template)
 		email_data['email'] = ''.join(to_emails)
 		html_content = template.render(email_data)
 		msg = EmailMessage(subject, html_content, 'Granite <' + from_email + '>', to_emails, headers={'Precedence': 'bulk'})
 		msg.content_subtype = "html"
 		msg.send()
+
+		
