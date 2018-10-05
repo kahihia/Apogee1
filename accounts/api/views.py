d076cf37a21ba85beda8c6351849470f1b1973b1
Malekagr <maa245@pitt.edu>
2018-10-05 14:58:17 -0400

    auth

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -8,8 +8,11 @@ class AuthorizationAPIView(APIView):
 		# party_event_type = party_qeryset.first().event_type
 		#if party_event is bid
 		account = UserProfile.objects.filter(email_auth_token=auth_key)
-		if account:
-			print("FOUND")
+		if account and account.is_authenticated == True:
+			account.is_authenticated = True
+			account.email_auth_token = ""
+			account.save(update_fields=['email_auth_token'])
+			account.save(update_fields=['is_authenticated'])
 			return Response(True)
 
 		print("Not Found")
