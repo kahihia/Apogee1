b5e2c76e86427e418931b950009238676f3abd4f
Malekagr <maa245@pitt.edu>
2018-10-05 14:42:16 -0400

    auth key

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,9 +1,15 @@
 from rest_framework.views import APIView
+from rest_framework.response import Response
 
 class AuthorizationAPIView(APIView):
 	def get(self, request, auth_key, format=None):
 		# party_qeryset = Party.objects.filter(pk=pk)
 		# party_event_type = party_qeryset.first().event_type
 		#if party_event is bid
-		print(auth_key)	
-		return Response()
+		account = User.profile.objects.filter(email_auth_token=auth_key)
+		if account:
+			print("FOUND")
+			return Response(True)
+
+		print("Not Found")
+		return Response(False)
