766f15395660b16e4afb278418098a4a1d34a02b
Malekagr <maa245@pitt.edu>
2018-10-05 14:46:32 -0400

    gogogo

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from accounts.models import UserProfile
 
 class AuthorizationAPIView(APIView):
 	def get(self, request, auth_key, format=None):
