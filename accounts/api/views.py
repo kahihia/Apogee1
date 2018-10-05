ee6614b28fc250b1ce2bf7050c107ac88186d7fa
Malekagr <maa245@pitt.edu>
2018-10-05 17:02:19 -0400

    print statements

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -7,16 +7,27 @@ class AuthorizationAPIView(APIView):
 		# party_qeryset = Party.objects.filter(pk=pk)
 		# party_event_type = party_qeryset.first().event_type
 		#if party_event is bid
+		print(1)
 		try:
+			print(2)
 			account = UserProfile.objects.get(email_auth_token=auth_key)
+			print(3)
 		except:
+			print(4)
 			return Response(False)
 
 		# account = account.first()
+		print(5)
 		if account and account.is_authenticated==False:
+			print(6)
 			account.is_authenticated = True
+			print(7)
 			account.email_auth_token = ""
+			print(8)
 			account.save(update_fields=['email_auth_token'])
+			print(9)
 			account.save(update_fields=['is_authenticated'])
+			print(10)
 			return Response(True)
+		print(11)
 		return Response(False)
