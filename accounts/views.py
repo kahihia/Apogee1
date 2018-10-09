eca2632b08945836210e0dac67e3d7f07f831a15
Malekagr <maa245@pitt.edu>
2018-10-09 12:11:11 -0400

    print on funds

--- accounts/views.py
+++ accounts/views.py
@@ -115,6 +115,8 @@ class FundsView(LoginRequiredMixin, DetailView):
     def get(self, request, *args, **kwargs):
         paypal_client_id = config("PAYPAL_CLIENT_ID", default="test"),
         paypal_env = config("PAYPAL_ENV", default="sandbox")
+        print("HERE IA MAMSDMSDMDADMDS")
+        print(self.request.user.profile.is_authenticated)
         if paypal_env == "live":
             paypal_env = "production"
         context = {'user': self.request.user, 'paypal_env': paypal_env, 'paypal_client_id': paypal_client_id[0] }
