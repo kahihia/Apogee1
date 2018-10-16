a2891c0b18c45cadc271c235f51139110c1a572c
Malekagr <maa245@pitt.edu>
2018-10-16 11:23:33 -0400

    changed redirect uri

--- accounts/views.py
+++ accounts/views.py
@@ -130,15 +130,12 @@ class UserTwitchAuthView(View, LoginRequiredMixin):
             'content-type': 'application/json',
         }
 
-        data = {"grant_type":"authorization_code","client_id": "f054futox6ybt8p07bndbqbuaw0v48","client_secret": "anu2ub103e0or8had2cn1h3d6yxtld","code": code,"redirect_uri": "https://malek-server.herokuapp.com"}
+        data = {"grant_type":"authorization_code","client_id": "f054futox6ybt8p07bndbqbuaw0v48","client_secret": "anu2ub103e0or8had2cn1h3d6yxtld","code": code,"redirect_uri": "https://malek-server.herokuapp.com/profiles/Tes/twitchauth/"}
 
         response = requests.post('https://id.twitch.tv/oauth2/token', headers=headers, data=data)
 
-        print(response)
-
-
-
-        print(code)
+        print(response.json)
+        print(response.text)
         return render(request, 'accounts/twitch_auth.html')
 # this is used to toggle following
 class UserFollowView(View, LoginRequiredMixin):
