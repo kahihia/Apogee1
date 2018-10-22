0d9431a14244aefd826c5bf89b168b7a886c1bd8
Malekagr <maa245@pitt.edu>
2018-10-22 09:44:41 -0400

    removed username from re-direct

--- accounts/views.py
+++ accounts/views.py
@@ -135,7 +135,7 @@ class UserTwitchAuthView(View, LoginRequiredMixin):
 
             data = {"grant_type":"authorization_code",'client_id': 'f054futox6ybt8p07bndbqbuaw0v48',
             "client_secret": "anu2ub103e0or8had2cn1h3d6yxtld","code":
-            code,"redirect_uri": "https://malek-server.herokuapp.com/profiles/Tes/twitchauth/"}
+            code,"redirect_uri": "https://malek-server.herokuapp.com/profiles/twitchauth/"}
             twitch_response = requests.post('https://id.twitch.tv/oauth2/token', headers=headers, data=json.dumps(data))
             twitch_dict=json.loads(twitch_response.text)
             # twitch_dict = twitch_functions.getOAuth(code)
