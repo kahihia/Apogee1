5172e36b2c88da155057f9a29ceaaf7a3e3c6e36
Malekagr <maa245@pitt.edu>
2018-11-14 12:42:55 -0500

    moved some print statements

--- parties/views.py
+++ parties/views.py
@@ -130,21 +130,17 @@ class PartyDetailView(DetailView):
 			try:
 				winners_list = qs.winners.all()
 				joined_list = qs.joined.all()
-				print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-				print("winners")
+				print("Winners:")
 				for w in winners_list: 
 					print(w)
-				print("joined")
+				print("Joined:")
 				context['place_in_queue'] = 'Not in queue'
 				count = 0
 				for j in joined_list:
 					count+=1
 					print(j.username)
-					print(self.request.user.username)
 					if str(j.username) == str(self.request.user.username):
-						print(str(count))
 						context['place_in_queue'] = count
-					print(j)
 				# context['place_in_queue'] = qs.joined_list.index(self.request.user)
 			except Exception as e: 
 				print(e)
