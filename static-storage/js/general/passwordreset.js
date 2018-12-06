994380b3abf42006555dd80eded91dcc94f67f14
Malekagr <maa245@pitt.edu>
2018-12-06 15:05:30 -0500

    Update passwordreset.js

--- static-storage/js/general/passwordreset.js
+++ static-storage/js/general/passwordreset.js
@@ -15,6 +15,11 @@ function addAuthenticationFunctionality(){
       			if(data.account_found==true){
       				$('#authentication_confirmation').text("	We have sent you a password reset token");
               jQuery('#email').remove();
+              jQuery('#email_btn').remove();
+              input = jQuery('<input size="6" type="text" id="password_token" name="password_token" value="" maxlength="6">');
+              jQuery('#div_auth_key').append(input);
+              input = jQuery('<input type="submit" value="Token" class="btn btn-primary-new ml-3" id ="token_btn">');
+              jQuery('#div_auth_key').append(input);
       			}
       			else{
       				$('#authentication_confirmation').text("	We could not find an account with that email address");   
