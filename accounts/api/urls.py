ebcf909a3e564bc692f2adc4433a6babf4f36024
Malekagr <maa245@pitt.edu>
2018-12-06 14:29:59 -0500

    Update urls.py

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -4,7 +4,8 @@ from django.views.generic import RedirectView
 # This import allows us to use event list API, which we need to 
 # display the events for the profile 
 from accounts.api.views import (
-		AuthorizationAPIView
+		AuthorizationAPIView,
+		PasswordResetAPIView
 	)
 from parties.api.views import (
 		PartyListAPIView
@@ -16,6 +17,6 @@ urlpatterns = [
 	# this url is the event API for the particular profile
     path('<username>/events/', PartyListAPIView.as_view(), name='list'),
     path('<auth_key>/authorization/', AuthorizationAPIView.as_view(), name='authorization'),
-    path('<email>/password_reset/', AuthorizationAPIView.as_view(), name='passwordreset')
+    path('<email>/password_reset/', PasswordResetAPIView.as_view(), name='passwordreset')
 
  ]
