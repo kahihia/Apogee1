51809207012986bd666711a504ea24f19b597016
Malekagr <maa245@pitt.edu>
2018-12-14 20:08:59 -0500

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -36,7 +36,7 @@ class UserRegisterView(FormView):
     # actually create user here. not sure why we do this, but i believe the cleaning
     # prevents some security issues
     def form_valid(self, form):
-        ref = self.request.POST.get('ref', 'None')
+        ref = json.loads(self.request.POST.get('ref', 'None'))
         # print(self.request.POST)
         print(self.request.GET)
         # print(self.request)
