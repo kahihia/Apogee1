9208666b2bed05bf2eb09b7c6f6eda2b42ee8f00
Caldwell Clarke <caldwell.s.clarke@gmail.com>
2019-01-22 15:48:32 -0500

    add streamlabs token refresh

--- apogee1/utils/streamlabs/streamlabs_functions.py
+++ apogee1/utils/streamlabs/streamlabs_functions.py
@@ -50,20 +50,29 @@ def get_streamlabs_details(code, user_obj):
 
 def refresh_streamlabs_credentials(user_obj):
 	try:
-		twitch_client_id = config('TWITCH_CLIENT_ID')
-		twitch_secret = config('TWITCH_SECRET')
-		twitch_redirect_uri = config('TWITCH_REDIRECT_URI')
-		params = (('grant_type', 'refresh_token'), ('refresh_token', user_obj.profile.twitch_refresh_token), ('Client-id', twitch_client_id), ('client_secret', twitch_secret),)
+		# Streamlabs auth step one. send back their code with our credentials to get user credentials
+		streamlabs_client_id = config('STREAMLABS_CLIENT_ID')
+		streamlabs_client_secret = config('STREAMLABS_CLIENT_SECRET')
+		streamlabs_redirect_uri = config('STREAMLABS_REDIRECT_URI')
 
-		response = requests.post('https://id.twitch.tv/oauth2/token?client_id='+twitch_client_id, params=params)
-		twitch_dict=json.loads(response.text)
-		print(twitch_dict)
-		user_obj.profile.twitch_refresh_token = twitch_dict['refresh_token']
-		user_obj.profile.twitch_OAuth_token = twitch_dict['access_token']
-		user_obj.profile.save(update_fields=['twitch_refresh_token'])
-		user_obj.profile.save(update_fields=['twitch_OAuth_token'])
+		data = {
+			"grant_type":"refresh_token",
+			'client_id': streamlabs_client_id,
+			'client_secret': streamlabs_client_secret,
+			"redirect_uri": streamlabs_redirect_uri, 
+			"refresh_token": user_obj.profile.streamlabs_refresh_token
+		}
+		url = "https://streamlabs.com/api/v1.0/token"
+		streamlabs_response = requests.requests("POST", url, params=data)
+		print(streamlabs_response.text)
+		streamlabs_dict=json.loads(streamlabs_response.text)
+		streamlabs_access_token = streamlabs_dict['access_token']
+		streamlabs_refresh_token = streamlabs_dict['refresh_token']
+
+		user_obj.profile.streamlabs_access_token = streamlabs_access_token
+		user_obj.profile.streamlabs_refresh_token = streamlabs_refresh_token
+		user_obj.profile.save(update_fields=['streamlabs_access_token'])
+		user_obj.profile.save(update_fields=['streamlabs_refresh_token'])
 		return True
 	except Exception as e:
-		print("refresh")
-		print(e)
 		return False
