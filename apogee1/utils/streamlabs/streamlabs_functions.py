ab8af9760016c95644027e615c121790df86569a
Caldwell Clarke <caldwell.s.clarke@gmail.com>
2019-01-29 16:55:02 -0500

    try v14

--- apogee1/utils/streamlabs/streamlabs_functions.py
+++ apogee1/utils/streamlabs/streamlabs_functions.py
@@ -32,8 +32,8 @@ def get_streamlabs_details(code, user_obj):
 		# url2 = '&client_secret='+streamlabs_client_secret+'&redirect_uri='+streamlabs_redirect_uri+'&code='+code
 		# url = url1 + url2
 		print('trying to post to streamlabs')
-		# streamlabs_response = requests.post(url, headers=headers, data=json.dumps(querydict))
-		streamlabs_response = requests.request("POST", url=url, params=querydict)
+		streamlabs_response = requests.post(url=url, data=json.dumps(querydict))
+		# streamlabs_response = requests.request("POST", url=url, params=querydict)
 		print('got a response')
 		print(streamlabs_response.text)
 		streamlabs_dict=json.loads(streamlabs_response.text)
