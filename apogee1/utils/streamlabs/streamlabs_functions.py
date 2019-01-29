c171ec4e8fa309dee95501ef03fbb919df5417e6
Caldwell Clarke <caldwell.s.clarke@gmail.com>
2019-01-29 16:12:33 -0500

    more data reformatting

--- apogee1/utils/streamlabs/streamlabs_functions.py
+++ apogee1/utils/streamlabs/streamlabs_functions.py
@@ -21,11 +21,11 @@ def get_streamlabs_details(code, user_obj):
 		# 	'Client-id': streamlabs_client_id
 		# }
 		querydict = {
-			'grant_type': 'authorization_code',
-			'client_id': streamlabs_client_id,
-			'client_secret': streamlabs_client_secret,
-			'redirect_uri': streamlabs_redirect_uri, 
-			'code': code,
+			'grant_type':'authorization_code',
+			'client_id':streamlabs_client_id,
+			'client_secret':streamlabs_client_secret,
+			'redirect_uri':streamlabs_redirect_uri, 
+			'code':code,
 		}
 		url = "https://streamlabs.com/api/v1.0/token"
 		# url1 = 'https://streamlabs.com/api/v1.0/token?grant_type=authorization_code&client_id='+streamlabs_client_id
